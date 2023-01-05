254b6ae8d9105342a20f78e5f60d39e739048961
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2023-01-05 03:21:48 +0100

    Ask a question

--- Assets/Scripts/NPCs/Asking.cs
+++ Assets/Scripts/NPCs/Asking.cs
@@ -1,17 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent (typeof(Speaking))]
 
-[System.Serializable]
 public class Asking : MonoBehaviour
 {
-    [SerializeField] private List<Question> questions = new List<Question> { };
+    [SerializeField] private Question question;
+
+    GameManager gameManager;
+    Button continueButton;
+    Text text;
+
+
+    bool running = false;
+
+    private void Awake()
+    {
+        gameManager = GameObject.FindWithTag("Game Manager").GetComponent<GameManager>();
+        text = gameManager.message;
+        continueButton = gameManager.continueButton;
+    }
 
     public void Ask(string msg)
     {
         GetComponent<Speaking>().Speak(msg);
+        continueButton.onClick.RemoveAllListeners();
+        continueButton.onClick.AddListener(AskQuestion);
+    }
+
+    private void AskQuestion()
+    {
+        text.text = "";
+        StartCoroutine(WriteQuestionsCoroutine());
+    }
+
+    private IEnumerator WriteQuestionsCoroutine()
+    {
+        foreach (string q in question.question)
+        {
+            text.text += "- ";
+
+            for (int i = 0; i < q.Length; i++)
+            {
+                text.text += q[i];
+
+                yield return new WaitForSeconds(.05f);
+            }
+            text.text += "\n";
+        }
     }
 }
 
