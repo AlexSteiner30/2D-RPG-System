925e29b30246bce9c7fe72fa0a808f20114c5545
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-01-15 22:15:15 +0100

    Inventory appear fix

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -26,10 +26,11 @@ public class Inventory : MonoBehaviour
         GameObject itemObject = new GameObject();
         itemObject.name = "Item";
 
+
         itemObject.AddComponent<Image>();
         itemObject.GetComponent<Image>().sprite = items[items.Count - 1].image;
-        itemObject.transform.localScale = new Vector2(1, 1);
-
+  
         itemObject.transform.parent = inventory.transform;
+        itemObject.transform.localScale = new Vector2(1, 1);
     }
 }
